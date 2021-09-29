2fcf13da145731cfc0a279ec3e092bdd2c8b133b
Andrew Hang <Andrewhang94@gmail.com>
2021-09-29 15:50:54 -0700

    fixed styles endpoint

--- server/model/model.js
+++ server/model/model.js
@@ -33,27 +33,34 @@ const querySingleProduct = async (id, callback) => {
 const queryStyles = async (id, callback) => {
   try {
     const result = await pool.query(`SELECT s.product_id, s.style_id, s.name, s.sale_price, s.original_price, s."default?", \
-  CASE WHEN count(p) = 0 THEN ARRAY[]::json[] ELSE array_agg(p.photos) END AS photos, \
-  json_object_agg(sku.sku_id, sku.sk) as skus \
-  FROM styles s \
-     LEFT JOIN \
-    ( \
-      SELECT photos.style_id, json_build_object('style_id', photos.style_id, 'url', photos.url, 'thumbnail_url', photos.thumbnail_url) as photos \
-      FROM photos \
-    ) p \
-      ON s.style_id = p.style_id \
+    CASE WHEN count(p) = 0 THEN ARRAY[]::json[] ELSE array_agg(p.photos) END AS photos, \
+    json_object_agg(sku.sku_id, sku.sk) as skus \
+    FROM styles s \
       LEFT JOIN \
       ( \
-        SELECT skus.style_id, skus.sku_id, json_build_object('size', size, 'quantity', quantity) as sk \
-        FROM skus \
-      ) sku \
-        ON sku.style_id = s.style_id \
-  WHERE s.product_id = '${id}' \
-  GROUP BY s.style_id`)
+        SELECT photos.style_id, json_build_object('style_id', photos.style_id, 'url', photos.url, 'thumbnail_url', photos.thumbnail_url) as photos \
+        FROM photos \
+      ) p \
+        ON s.style_id = p.style_id \
+        INNER JOIN \
+        ( \
+          SELECT skus.style_id, skus.sku_id, json_build_object('size', size, 'quantity', quantity) as sk \
+          FROM skus \
+        ) sku \
+          ON sku.style_id = s.style_id \
+    WHERE s.product_id = '${id}' \
+    GROUP BY s.style_id`)
 
     const formatResult = {
       product_id: id,
       results: result.rows,
+    }
+
+    if (result.rows.length === 0) {
+      const styles = await pool.query(`SELECT s.product_id, s.style_id, s.name, s.sale_price, s.original_price, s."default?", '[]'::json as photos, '{}'::json as skus \
+      FROM styles s \
+      WHERE s.product_id = '${id}'`)
+      formatResult.results = styles.rows
     }
 
     callback(null, formatResult);
