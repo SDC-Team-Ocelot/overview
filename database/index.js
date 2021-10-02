c0d31a075977e637e0f5705ff92b34de7575a724
Andrew Hang <Andrewhang94@gmail.com>
2021-10-02 15:50:45 -0700

    edit postgres server info

--- database/index.js
+++ database/index.js
@@ -3,13 +3,23 @@ require('dotenv').config();
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: 'postgres', // <-- for docker user for local is andrewhang
   password: process.env.DB_PASSWORD,
-  database: 'overview',
-  host: 'host.docker.internal',
-  port: 1234
+  database: 'postgres', // <-- for docker db for local is overview
+  host: 'localhost',
+  port: 5432
 });
 
 
 module.exports = pool;
 
+
+// Docker
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: 'overview',
+//   host: 'host.docker.internal',
+//   port: 1234
+// });
